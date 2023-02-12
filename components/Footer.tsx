4c9d54272301f316f1205ff42d697c2b2958ac63
The73756 <the73756the@gmail.com>
2023-02-12 13:43:55 +0900

    finish footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,18 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mt-6 hidden xl:block">
+      <p className="text-sm text-gray-400">
+        Created by{' '}
+        <a className="footerLink" href="https://github.com/The73756">
+          @The73756
+        </a>{' '}
+        with{' '}
+        <a className="footerLink" href="https://nextjs.org/">
+          Next.js
+        </a>
+      </p>
+    </footer>
+  );
 };
 
 export default Footer;
