3d9644f31ef94ad2c35c6be8743a03d124b6cf85
The73756 <the73756the@gmail.com>
2023-03-18 13:33:10 +0900

    upd NoResults component

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,8 +1,19 @@
+import { MdOutlineVideocamOff } from 'react-icons/md';
+import { BiCommentX } from 'react-icons/bi';
+
 interface INoResultsProps {
   text: string;
+  type: 'video' | 'comment';
 }
-const NoResults = ({ text }: INoResultsProps) => {
-  return <div>{text}</div>;
+const NoResults = ({ text, type }: INoResultsProps) => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center">
+      <span className="text-6xl">
+        {type === 'video' ? <MdOutlineVideocamOff /> : <BiCommentX />}
+      </span>
+      <span className="text-center text-2xl">{text}</span>
+    </div>
+  );
 };
 
 export default NoResults;
